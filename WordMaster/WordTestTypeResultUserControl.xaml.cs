d0166f47c59fbaf6f214b4d31242ee012d28bacd
Patrick Rećko <guest_none@protonmail.com>
2020-01-04 21:50:47 +0100

    WIP: Add initial windows and user control skeletons for the UI

---
+++ WordMaster/WordTestTypeResultUserControl.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WordMaster
+{
+
+    public enum ResultType
+    {
+        Correct,
+        Incorrect
+    }
+
+    /// <summary>
+    /// Interaction logic for WordTestTypeResultUserControl.xaml
+    /// </summary>
+    public partial class WordTestTypeResultUserControl : UserControl
+    {
+        public WordTestTypeResultUserControl(ResultType resultType, string properWord)
+        {
+            InitializeComponent();
+            if (resultType == ResultType.Correct)
+            {
+                resultLabel.Content = "Correct";
+                resultLabel.Background = Brushes.LightGreen;
+            }
+            else
+            {
+                resultLabel.Content = "Incorrect, the proper word is: " + properWord;
+                resultLabel.Background = Brushes.Red;
+            }
+        }
+    }
+}
---
+++ WordMaster/WordTestWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WordMaster
+{
+
+
+    /// <summary>
+    /// Interaction logic for WordTestWindow.xaml
+    /// </summary>
+    public partial class WordTestWindow : Window
+    {
+        public WordTestWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
